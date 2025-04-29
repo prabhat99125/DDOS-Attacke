73e78c679391b20ac15171b2c15f5e22003a4253
Prabhat Rathva <144404271+prabhat99125@users.noreply.github.com>
2025-04-29 21:50:52 +0530

    Update multi-launch.js

--- multi-launch.js
+++ multi-launch.js
@@ -1,7 +1,7 @@
 // multi-launch.js
 const { fork } = require("child_process");
 
-const instances = 2; // 1 per vCPU (safe for your VPS)
+const instances = 6; // 1 per vCPU (safe for your VPS)
 
 for (let i = 0; i < instances; i++) {
   fork("./app.js");
